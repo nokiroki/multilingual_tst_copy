2b71f91e0f9675c974e59d8a7b1ecdbdcac2896c
elizacc <49486233+elizacc@users.noreply.github.com>
2022-05-25 16:41:33 +0300

    Update xlm-r_score.py

--- classifier/xlm-r_score.py
+++ classifier/xlm-r_score.py
@@ -26,7 +26,7 @@ def main():
     parser.add_argument('-lang', default='en_XX', type=str, help='the name of language')
     parser.add_argument('-model_dir', default='checkpoint/', type=str, help='model_dir')
     parser.add_argument('-seed', default=42, type=int, help='pseudo random number seed')
-    parser.add_argument('-dataset', default='xformal', type=str, help='the dataset name')
+    parser.add_argument('-dataset', default='detox', type=str, help='the dataset name')
     parser.add_argument('-batch_size', default=256, type=int, help='max sent. in a batch')
 
     opt = parser.parse_args()
